7dc2977a94f125eae377ed3d8e7d5a33fd3d0084
kendrickbrayman <52048642+kendrickbrayman@users.noreply.github.com>
2019-12-03 17:50:44 -0800

    Update stats101C final project.R

--- stats101C final project.R
+++ stats101C final project.R
@@ -5,7 +5,7 @@ y <- training$HTWins
 y <- ifelse(y == "Yes",T,F)
 
 x <- training[,-c(1,2,3,4,5,6,8,which(duplicated(t(training[1,]))))]
-
+testx <- test[,-c(1:7,which(duplicated(t(training[1,]))))]
 createsol <- function(name,model){
   probs <- predict(model,newdata = test)
   answers <- cbind(test[,1],ifelse(probs > 0.5,"Yes","No"))
@@ -20,7 +20,7 @@ model1 <- glm(y~., data = x)
 summary(model1)
 createsol("submission1.csv",model1)
 
-#submission2. only points allowed/scored (cheese didnt work) accuract 0.6009
+#submission2. only points allowed/scored (cheese didnt work) accuraty 0.6009
 
 model2 <- glm(y~VT.TA.pts+VT.TS.pts,data=x)
 
@@ -37,3 +37,64 @@ out <-ifelse(((test$VT.TS.fgm*2 + test$VT.TS.tpm*3) - (test$HT.TS.tpm * 3 + test
 out <- cbind(test$id,out)
 colnames(out) <- c('id','HTWins')
 write.csv(out,"submission4.csv",row.names = F)
+
+#bigSTDmat
+big <- rbind(matrix(x),matrix(testx))
+
+bigstd <- big %>% apply(2,function(x){(x - mean(x))/sd(x)})
+
+trainSTD <- bigstd[1:9520,]
+testSTD <- bigstd[-c(1:9520),]
+
+
+#submission5 RIDGE 0.618
+library(glmnet)
+library(tidyr)
+lambdas <- 10^seq(3, -2, by = -.1)
+model5 <- cv.glmnet(trainSTD,y,alpha = 0, lambda = lambdas)
+
+minl <- model5$lambda.min
+
+outmodel <- glmnet(x2,y2,alpha = 0, lambda = minl,family = 'binomial')
+probs <- predict(outmodel,newx = data.matrix(testSTD))
+answers <- cbind(test[,1],ifelse(probs > 0.5,"Yes","No"))
+colnames(answers) <- c("id","HTWins")
+write.csv(answers,"submission5.csv",row.names = F)
+
+#submission6 LASSO 0.625
+model6 <- cv.glmnet(trainSTD,y,alpha = 1, lambda = lambdas)
+
+minl <- model6$lambda.min
+
+outmodel <- glmnet(x2,y2,alpha = 1, lambda = minl,family = 'binomial')
+probs <- predict(outmodel,newx = data.matrix(testSTD))
+answers <- cbind(test[,1],ifelse(probs > 0.5,"Yes","No"))
+colnames(answers) <- c("id","HTWins")
+write.csv(answers,"submission6-2.csv",row.names = F)
+
+#submission7 MIXED NOT SUBMITTED
+model7 <- cv.glmnet(trainSTD,y,alpha = 0.2, lambda = lambdas)
+
+minl <- model7$lambda.min
+
+outmodel <- glmnet(x2,y2,alpha = 0, lambda = minl,family = 'binomial')
+probs <- predict(outmodel,newx = data.matrix(testSTD))
+answers <- cbind(test[,1],ifelse(probs > 0.5,"Yes","No"))
+colnames(answers) <- c("id","HTWins")
+write.csv(answers,"submission6.csv",row.names = F)
+
+#submission 8 PCA then Knn NOT SUBMITTED
+
+trainPC <- prcomp(big[1:9520,],center = T,scale = T)
+probs <- predict(trainPC,newdata = big[-c(1:9520),])
+library(class)
+ans <- knn(trainPC$x,probs,y)
+answers <- cbind(test[,1],ifelse(as.logical(ans),"Yes","No"))
+colnames(answers) <- c("id","HTWins")
+write.csv(answers,"submission8.csv",row.names = F)
+
+#winrates
+
+for(i in names(summary(training$VT))){
+  cat(i,sum(training$HT == i & training$HTWins == "Yes")/sum(training$HT == i),'\n')
+}
